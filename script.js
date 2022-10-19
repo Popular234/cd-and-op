46a2dc18acfba240a44fc168c8be7b7451a47a7b
Popular234 <cainjose24@g.crookcountyschools.org>
2022-10-19 14:32:13 -0700

    hey bro

---
+++ script.js
@@ -0,0 +1,19 @@
+// console.log("hello world")
+
+var num = prompt("Hey, on a scale of 1 to 5 how are you feeling today? (just a number, no explaning necessary)")
+
+if(num == 1){
+    alert('Im sorry you feel that way, I hope it gets better.')
+}
+else if(num == 2){
+    alert('Im sorry you feel that way, I hope it gets better.')
+}
+else if(num == 3){
+    alert('not too bad, it can still get better')
+}
+else if(num == 4){
+    alert('not too bad, it can still get better')
+}
+else{
+    alert('Thats great, maybe spread some good vibes around')
+};
